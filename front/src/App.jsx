8e409a1c874beae0560c1ef5433d3fac38bfae23
Luis Miguel <luis.hernandezbermeo@estudiantesunibague.edu.co>
2026-05-11 10:11:43 -0500

    fin del dia

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,36 @@
-
+import { useState, useEffect } from "react"
+import { io} from "socket.io-client"
 
 function App( ){
+  const [inputMessage, setInputMessage] = useState("")
+  const [ mensajeRecibido, setmensajeRecibido] = useState([])
+  const [ socket, setSocket ] = useState() 
 
-  const [inputMessaje, setInputMessage] = useState("")
+useEffect (()=>{
+  const newSocket = io("localhost:3000")
+  setSocket(newSocket) 
+
+  newSocket.on("mensaje", (msg) => {
+    setmensajeRecibido (msg) 
+  })
+
+  return( ) => {newSocket.disconnect() }
+}, [])
 
   const handleSubmit = (e) => {
     e.preventDefault( )
     // como se envian los mensajes.....
+    socket.emit("mensaje", inputMessage)
   }
 
   return (
     <div>
-      <from onSubmit={handSubmit}>
+      <form onSubmit={handleSubmit}>
         <input onChange={(e) => setInputMessage(e.target.value)}/> 
         <button type="submit">Enviar</button>
-      </from>
+      </form>
+      { mensajeRecibido.map ( mensaje => <div> {mensaje}</div>) }
+      { mensajeRecibido } 
     </div>
   ) 
 }
